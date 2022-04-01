5573eddab3cac18e88613ff44b4014dab339606d
Laia Markixa <laiamr77@gmail.com>
2022-04-01 11:49:17 +0200

    adding styles to splash screen

--- splash.js
+++
@@ -1,7 +0,0 @@
-/* background canvas */
-
-const canvas = document.querySelector("#bubbles");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext("2d"); 
-
